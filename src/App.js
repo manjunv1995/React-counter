5c9286f78dbec9e4e188319063cf38508b369ac0
manjunath vijapuram <vmmanjunathece@gmail.com>
2020-01-26 17:02:17 +0530

    some thing error

--- src/App.js
+++ src/App.js
@@ -1,18 +1,54 @@
-import React from "react";
-import logo from "./logo.svg";
+import React, { Component } from "react";
+import NavBar from "./components/navbar";
 import "./App.css";
+import Counters from "./components/counters";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    counters: [
+      { id: 1, value: 4 },
+      { id: 2, value: 0 },
+      { id: 3, value: 0 },
+      { id: 4, value: 0 }
+    ]
+  };
+
+  handleReset = () => {
+    this.setState({
+      counters: this.state.counters.map(el => {
+        el.value = 0;
+        return el;
+      })
+    });
+  };
+
+  handleDelete = counterId => {
+    this.setState({
+      counters: this.state.counters.filter(c => {
+        return c.id !== counterId.id;
+      })
+    });
+  };
+
+  render() {
+    return (
+      <React.Fragment>
+        <NavBar
+          totalCounters={console.log(
+            this.state.counters.filter(c => c.value > 0).length
+          )}
+        />
+        <main className="container">
+          <Counters
+            counters={this.state.counters}
+            onReset={this.handleReset}
+            onIncrement={this.handleIncrement}
+            onDelete={this.handleDelete}
+          />
+        </main>
+      </React.Fragment>
+    );
+  }
 }
 
 export default App;
--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -1,45 +1,57 @@
+// import React, { Component, Fragment } from "react";
+// import Counter from "./counter";
+
+// class Counters extends Component {
+//   state = {
+//     counters: [
+//       { id: 1, value: 4 },
+//       { id: 2, value: 0 },
+//       { id: 3, value: 0 },
+//       { id: 4, value: 0 }
+//     ]
+//   };
+
+//   handleDelete = counterId => {
+//     this.setState({
+//       counters: this.state.counters.filter(el => {
+//         return el.id !== counterId.id;
+//       })
+//     });
+//   };
+//   render() {
+//     return (
+//       <div>
+//         {this.state.counters.map(counter => (
+//           <Counter
+//             key={counter.id}
+//             onDelete={() => this.handleDelete(counter)}
+//             value={counter.value}
+//           />
+//         ))}
+//       </div>
+//     );
+//   }
+// }
+
+// export default Counters;
+
 import React, { Component, Fragment } from "react";
 import Counter from "./counter";
 
 class Counters extends Component {
-  state = {
-    counters: [
-      { id: 1, value: 4 },
-      { id: 2, value: 0 },
-      { id: 3, value: 0 },
-      { id: 4, value: 0 }
-    ]
-  };
-
-  onReset = () => {
-    this.setState({
-      counters: this.state.counters.map(el => {
-        el.value = 0;
-        return el;
-      })
-    });
-  };
-
-  handleDelete = counterId => {
-    this.setState({
-      counters: this.state.counters.filter(el => {
-        return el.id !== counterId.id;
-      })
-    });
-  };
   render() {
     return (
       <div>
         <button
-          onClick={() => this.onReset()}
+          onClick={this.props.onReset}
           className="btn btn-primary m-2 btn-sm"
         >
           Reset
         </button>
-        {this.state.counters.map(counter => (
+        {this.props.counters.map(counter => (
           <Counter
             key={counter.id}
-            onDelete={() => this.handleDelete(counter)}
+            onDelete={this.props.onDelete}
             value={counter.value}
           />
         ))}
